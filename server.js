db13077610accae3603a817b8a18c142a1bf3b6a
ggchan0 <ggchan@calpoly.edu>
2015-10-11 03:39:18 -0700

    static webserver

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("hello world!");
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname +'/public'));
+
+app.listen(3000, function(err){
+   if (err) {
+      console.log(err);
+   } else {
+      console.log('runnin');
+   }
+});
